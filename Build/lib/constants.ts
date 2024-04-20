0277bdd37148d452884d636d2e077ff684aa67da
cui0wei0 <14077851+cui0wei0@users.noreply.github.com>
2024-04-21 02:22:58 +0800

    Delete Build directory

--- Build/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export const SHARED_DESCRIPTION = [
-  'License: AGPL 3.0',
-  'Homepage: https://ruleset.skk.moe',
-  'GitHub: https://github.com/SukkaW/Surge'
-] as const;
--- Build/lib/memo-promise.ts
+++
@@ -1,8 +0,0 @@
-export const createMemoizedPromise = <T>(fn: () => Promise<T>, preload = true): () => Promise<T> => {
-  let promise: Promise<T> | null = preload ? fn() : null;
-
-  return () => {
-    promise ??= fn();
-    return promise;
-  };
-};
